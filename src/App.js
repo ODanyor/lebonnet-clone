eeb9bebb82852db01cb954ade0d2affc3550fc9a
Danyor <dubashit@bk.ru>
2020-02-17 01:24:10 +0500

    Token managing

--- src/App.js
+++ src/App.js
@@ -10,16 +10,35 @@ import AccountPage from "./pages/AccountPage";
 import ProductsPage from "./pages/ProductsPage";
 import ProductPage from "./pages/ProductPage";
 
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+// Redux
+import store from "./redux/store";
+import { SET_AUTHENTICATED } from "./redux/types";
+import { logOut } from "./redux/actions/userActions";
+
+const token = localStorage.Token;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    store.dispatch(logOut());
+    window.location.href = "/signIn";
+  } else {
+    store.dispatch({ type: SET_AUTHENTICATED });
+    axios.defaults.headers.common["Authorization"] = token;
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/account" component={AccountPage} />
-          <Route exact path="/products" component={ProductsPage} />
-          <Route exact path={`/product`} component={ProductPage} />
+          <Route exact strick path="/" component={HomePage} />
+          <Route exact strick path="/account" component={AccountPage} />
+          <Route exact strick path="/products" component={ProductsPage} />
+          <Route exact strick path="/product" component={ProductPage} />
         </Switch>
         <Footer />
       </Router>
