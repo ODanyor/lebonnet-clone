4b7a615bd7974e4cd8dda3f4ab5b626dde86ed23
Dany <dubashit@bk.ru>
2020-02-28 15:59:16 -0500

    Slider feature

--- src/layouts/slider/Slider.js
+++ src/layouts/slider/Slider.js
@@ -1,95 +1,69 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../../static/styles/Slider.css";
-const slider_img_1 = require("../../static/images/slider/slider_img_1.png");
-const slider_img_2 = require("../../static/images/slider/slider_img_2.png");
+const slider_img_1 = require("../../static/images/slider/slider_img_1.jpg");
+const slider_img_2 = require("../../static/images/slider/slider_img_2.jpg");
 
-class Slider extends React.Component {
-  constructor(props) {
-    super(props);
+function Slider() {
+  const [seconds, setSeconds] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [sliders, setSliders] = useState([
+    { topic: "Winter is coming", img: slider_img_1 },
+    { topic: "100% biodegradable", img: slider_img_2 }
+  ]);
 
-    this.state = {
-      seconds: 0,
-      counter: 0,
-      slides: []
-    };
-  }
-
-  UNSAFE_componentWillMount() {
-    console.log("First");
-    this.setState(prevState => ({
-      ...prevState,
-      slides: [
-        { topic: "Winter is coming", img: slider_img_1 },
-        { topic: "100% biodegradable", img: slider_img_2 }
-      ]
-    }));
-  }
-  // componentDidMount() {
-  //   let interval = null;
-  //   interval = setInterval(() => {
-  //     this.setState(prevState => prevState.seconds + 1);
-  //   }, 1000);
-  //   if (this.state.seconds !== 0) {
-  //     clearInterval(interval);
-  //   }
-  //   return () => clearInterval(interval);
-  // }
+  useEffect(() => {
+    let interval = null;
+    interval = setInterval(() => {
+      setSeconds(prevState => prevState + 1);
+    }, 1000);
+    if (seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, []);
   // "slideOut 1.3s ease";
   // "slideIn 1s ease";
 
-  // reset = () => {
-  //   this.setState(prevState => ({
-  //     ...prevState,
-  //     seconds: 0
-  //   }));
-  // };
+  const reset = () => {
+    setSeconds(0);
+  };
 
-  // slider = () => {
-  //   console.log("Slider is running ...");
-  //   if (this.state.seconds === 4) {
-  //     document.querySelector("#img").style.animation = "slideOut 1.1s ease";
-  //   }
-  //   if (this.state.seconds === 5) {
-  //     document.querySelector("#img").style.animation = "slideIn 1s ease";
-  //     this.reset();
-  //     this.setState(prevState => prevState.counter + 1);
-  //     if (this.state.counter === this.state.slides.length - 1) {
-  //       this.setState(prevState => ({
-  //         ...prevState,
-  //         counter: 0
-  //       }));
-  //     }
-  //   }
-  // };
-
-  render() {
-    return (
-      <div className="SlideContainer">
-        <div className="Slide">
-          <Link className="link" to="/">
-            <img
-              id="img"
-              className="SlideIn"
-              alt="slide"
-              src={this.state.slides[this.state.counter].img}
-              width="100%"
-              height="auto"
-            />
-            <div>
-              <span className="Span">
-                {this.state.slides[this.state.counter].topic}
-              </span>
-              <span className="Span">We got you covered &rarr;</span>
-            </div>
-          </Link>
-        </div>
-        {/* Slide map indicator */}
-      </div>
-    );
+  if (seconds === 4) {
+    document.querySelector("#img").style.animation = "slideOut 1.1s ease";
   }
+  if (seconds === 5) {
+    document.querySelector("#img").style.animation = "slideIn 1s ease";
+    reset();
+    setCounter(prevState => prevState + 1);
+    if (counter === sliders.length - 1) {
+      setCounter(0);
+    }
+  }
+
+  return (
+    <div className="SlideContainer">
+      <div className="Slide">
+        <Link className="link" to="/">
+          <img
+            id="img"
+            className="SlideIn"
+            alt="slide"
+            src={sliders[counter].img}
+            width="100%"
+            height="auto"
+          />
+          <div>
+            <span className="Span">{sliders[counter].topic}</span>
+            <span className="Span">We got you covered &rarr;</span>
+          </div>
+        </Link>
+      </div>
+      {/* Slide map indicator */}
+    </div>
+  );
 }
 
 export default Slider;
